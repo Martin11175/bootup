b12352e24d7eb5c19536bc8fe0a77572bacb952b
Martin Higgs <martinhiggs85@googlemail.com>
2014-11-24 20:29:32 +0000

    Database encoded. Project begins...

--- controllers/default.py
+++ controllers/default.py
@@ -1,2 +1,22 @@
 def index():
+    """
+    5 most recently created projects (by ID)
+    5 projects closest to their goal (heavy calculation to sum all pledges, consider revising.)
+    """
+    return dict()
+
+
+def signup():
+    """
+    Form for creating a new user. Requires:
+    UID, name, dob, complete address (street addr, city, country, post),
+            credit card (ID, expiry, PIN, address)
+    Mainly duplicate addresses; new table
+    """
+    return dict()
+
+def bootgoup():
+    """
+    Form for creating bootable. Must be logged in first.
+    """
     return dict()
--- models/db.py
+++ models/db.py
@@ -1,87 +1,44 @@
 # -*- coding: utf-8 -*-
+from gluon import DAL, Field
 
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-## File is released under public domain and you can use without limitations
-#########################################################################
+db = DAL('sqlite://bootup.db')
+db.define_table('users',
+                Field('username', 'string', required=True, notnull=True, unique=True),
+                Field('pwd', 'password', required=True, notnull=True),
+                Field('firstname', 'string', required=True, notnull=True),
+                Field('lastname', 'string', required=True, notnull=True),
+                Field('dob', 'date', required=True, notnull=True),
+                Field('address_ref', 'reference address', required=True, notnull=True),
+                Field('credit_ref', 'reference credit', required=True, notnull=True))
+db.define_table('address',
+                Field('number', 'string', required=True, notnull=True),
+                Field('street', 'string', required=True, notnull=True),
+                Field('city', 'string', required=True, notnull=True),
+                Field('country', 'string', required=True, notnull=True),
+                Field('postcode', 'string', required=True, notnull=True))
+db.define_table('credit',
+                Field('number', 'integer', required=True, notnull=True),
+                Field('expiry', 'date', required=True, notnull=True),
+                Field('PIN', 'integer', required=True, notnull=True),
+                Field('address_ref', 'reference address', required=True, notnull=True))
+db.define_table('bootables',
+                Field('title', 'string', required=True, notnull=True),
+                Field('intro', 'text', required=True, notnull=True),
+                Field('category_ref', 'reference categories', required=True, notnull=True),
+                Field('goal', 'integer', required=True, notnull=True),
+                Field('image', 'upload', uploadfield='image_file', required=True, notnull=True),
+                Field('image_file', 'blob', required=True, notnull=True),
+                Field('desc', 'text', required=True, notnull=True),
+                Field('about_us', 'text', required=True, notnull=True),
+                Field('status', 'string', required=True, notnull=True, default='NOT_AVAILABLE'),
+                Field('boot_manager', 'reference users', required=True, notnull=True))
+db.define_table('categories',
+                Field('name', 'string', required=True, notnull=True))
+db.define_table('pledges',
+                Field('boot_ref', 'reference bootables', required=True, notnull=True),
+                Field('value', 'integer', required=True, notnull=True),
+                Field('reward', 'text', required=True, notnull=True))
+db.define_table('pledged',
+                Field('pledge_ref', 'reference pledges', required=True, required=True),
+                Field('user_ref', 'reference users', required=True, required=True))
 
-## if SSL/HTTPS is properly configured and you want all HTTP requests to
-## be redirected to HTTPS, uncomment the line below:
-# request.requires_https()
-
-if not request.env.web2py_runtime_gae:
-    ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
-else:
-    ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore+ndb')
-    ## store sessions and tickets there
-    session.connect(request, response, db=db)
-    ## or store session in Memcache, Redis, etc.
-    ## from gluon.contrib.memdb import MEMDB
-    ## from google.appengine.api.memcache import Client
-    ## session.connect(request, response, db = MEMDB(Client()))
-
-## by default give a view/generic.extension to all actions from localhost
-## none otherwise. a pattern can be 'controller/function.extension'
-response.generic_patterns = ['*'] if request.is_local else []
-
-## (optional) optimize handling of static files
-# response.optimize_css = 'concat,minify,inline'
-# response.optimize_js = 'concat,minify,inline'
-## (optional) static assets folder versioning
-# response.static_version = '0.0.0'
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - old style crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
-
-from gluon.tools import Auth, Service, PluginManager
-
-auth = Auth(db)
-service = Service()
-plugins = PluginManager()
-
-## create all tables needed by auth if not custom tables
-auth.define_tables(username=False, signature=False)
-
-## configure email
-mail = auth.settings.mailer
-mail.settings.server = 'logging' if request.is_local else 'smtp.gmail.com:587'
-mail.settings.sender = 'you@gmail.com'
-mail.settings.login = 'username:password'
-
-## configure auth policy
-auth.settings.registration_requires_verification = False
-auth.settings.registration_requires_approval = False
-auth.settings.reset_password_requires_verification = True
-
-## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
-## register with janrain.com, write your domain:api_key in private/janrain.key
-from gluon.contrib.login_methods.janrain_account import use_janrain
-use_janrain(auth, filename='private/janrain.key')
-
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
-
-## after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
