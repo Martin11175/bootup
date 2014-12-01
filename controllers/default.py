0bdb4e245d7eaa7a3f61d15234cb83e90a89102e
Martin Higgs <martinhiggs85@googlemail.com>
2014-12-01 20:22:29 +0000

    Completed fundamental CSS.

--- controllers/default.py
+++ controllers/default.py
@@ -32,7 +32,7 @@ def index():
                           & (db.bootables.id == db.pledges.boot_ref)
                           & (db.pledges.id == db.pledged.pledge_ref))\
             .select(db.bootables.ALL, totals, groupby=db.bootables.id, orderby=~totals,
-                    having=((db.bootables.goal - totals) > 0), limitby=(0, 5)):
+                    having=(db.bootables.goal > totals), limitby=(0, 5)):
         bootable = Storage()
         bootable['title'] = A(db_bootable.bootables.title, callback=URL('view_boot', args=db_bootable.bootables.id))
         bootable['category'] = db.categories[db_bootable.bootables.category_ref].name
@@ -63,86 +63,113 @@ def signup():
     # Break the form down into chunks to reduce visual load, don't start personal info until after username
     if (request.args(0) == '1') and 'username' in session.signup:
         session.current_signup_page = 1
-        form = FORM(H3('Just a little more!'),
-                    H4('Awesome! We\'re gonna need some personal details to get you booting.'),
-                    DIV(LABEL('First Name', _for='firstname'),
-                        INPUT(_name='firstname', requires=IS_NOT_EMPTY(),
-                              _value=session.signup['firstname'] if 'firstname' in session.signup else '')),
-                    DIV(LABEL('Last Name', _for='lastname'),
-                        INPUT(_name='lastname', requires=IS_NOT_EMPTY(),
-                              _value=session.signup['lastname'] if 'lastname' in session.signup else '')),
-                    DIV(LABEL('Date of Birth', _for='dob'),
-                        INPUT(_name='dob', _type='date',
-                              requires=IS_DATE_IN_RANGE(maximum=date.today()),
-                              _value=session.signup['dob'] if 'dob' in session.signup else '')),
+        form = FORM(H2('Just a little more!'),
+                    H3('Awesome! We\'re gonna need some personal details to get you booting.'),
+                    DIV(H4('About you'),
+                        DIV(LABEL('First Name', _for='firstname'),
+                            INPUT(_name='firstname', requires=IS_NOT_EMPTY(error_message='Please enter your first name.'),
+                                  _value=session.signup['firstname'] if 'firstname' in session.signup else '')),
+                        DIV(LABEL('Last Name', _for='lastname'),
+                            INPUT(_name='lastname', requires=IS_NOT_EMPTY(error_message='Please enter your last name.'),
+                                  _value=session.signup['lastname'] if 'lastname' in session.signup else '')),
+                        DIV(LABEL('Date of Birth', _for='dob'),
+                            INPUT(_name='dob', _type='date',
+                                  requires=IS_DATE_IN_RANGE(maximum=date.today(),
+                                        error_message='Please enter your date of birth (cannot be after today).'),
+                                  _value=session.signup['dob'] if 'dob' in session.signup else '')),
+                        _class='info_div'),
                     DIV(H4('Address'),
                         DIV(LABEL('House Name / Number', _for='house'),
-                            INPUT(_name='house', requires=IS_NOT_EMPTY(),
+                            INPUT(_name='house',
+                                  requires=IS_NOT_EMPTY(error_message='Please enter your house name / number.'),
                                   _value=session.signup['address.house'] if 'address.house' in session.signup else '')),
                         DIV(LABEL('Road name', _for='street'),
-                            INPUT(_name='street', requires=IS_NOT_EMPTY(),
+                            INPUT(_name='street', requires=IS_NOT_EMPTY('Please enter your road name.'),
                                   _value=session.signup['address.street'] if 'address.street' in session.signup else '')),
                         DIV(LABEL('City', _for='city'),
-                            INPUT(_name='city', requires=IS_NOT_EMPTY(),
+                            INPUT(_name='city', requires=IS_NOT_EMPTY('Please give the city location of your address.'),
                                   _value=session.signup['address.city'] if 'address.city' in session.signup else '')),
                         DIV(LABEL('Country', _for='country'),
-                            INPUT(_name='country', requires=IS_NOT_EMPTY(),
+                            INPUT(_name='country', requires=IS_NOT_EMPTY('Please enter a country.'),
                         _value=session.signup['address.country'] if 'address.country' in session.signup else '')),
                         DIV(LABEL('Post Code', _for='postcode'),
-                            INPUT(_name='postcode', requires=IS_MATCH('^\w{4} \w{3}$'),
-                                  _value=session.signup['address.postcode'] if 'address.postcode' in session.signup else ''))),
-                    DIV(INPUT(_type='submit', _value='Next')))
-        form.add_button('Prev', URL('signup', args=(session.current_signup_page - 1)))
+                            INPUT(_name='postcode', requires=IS_MATCH('^\w{4} \w{3}$',
+                                        error_message='Please enter your postcode (like "YO12 5ZB").'),
+                                  _value=session.signup['address.postcode'] if 'address.postcode' in session.signup else '')),
+                        _class='address_div'),
+                    DIV(_class='clear'),
+                    INPUT(_type='submit', _value='Next', _class='button_forward'),
+                    _id='split_form')
+        form.add_button('Prev', URL('signup', args=(session.current_signup_page - 1)), _class="button_back")
 
     # Finish up with credit information if personal details are complete
     elif (request.args(0) == '2') and 'address.postcode' in session.signup:
         session.current_signup_page = 2
-        form = FORM(H3('One. Last. Step.'),
-                    H4('Finally some credit information for the bootables you love.'),
-                    DIV(LABEL('Card Number', _for='card_num'),
-                        INPUT(_name='card_num', _type='number', requires=IS_MATCH('^\d{12}$'))),
-                    DIV(LABEL('Card Expiry Date', _for='expiry'),
-                        INPUT(_name='expiry', _type='month', requires=IS_DATE_IN_RANGE(format='%Y-%m',
-                                                                                       minimum=date.today()))),
-                    DIV(LABEL('Security PIN', _for='pin'),
-                        INPUT(_name='pin', _type='number', _min='000', _max='999', requires=IS_MATCH('^\d{3}$'))),
+        form = FORM(H2('One. Last. Step.'),
+                    H3('Finally some credit information for the bootables you love.'),
+                    DIV(H4('Card details'),
+                        DIV(LABEL('Card Number', _for='card_num'),
+                            INPUT(_name='card_num', _type='number',
+                                  requires=IS_MATCH('^\d{12}$', error_message='Please enter a 12 digit card number.'))),
+                        DIV(LABEL('Card Expiry Date', _for='expiry'),
+                            INPUT(_name='expiry', _type='month',
+                                  requires=IS_DATE_IN_RANGE(format='%Y-%m', minimum=date.today(),
+                                        error_message='Please enter your card\'s expiry date (e.g. 2015-02).'))),
+                        DIV(LABEL('Security PIN', _for='pin'),
+                            INPUT(_name='pin', _type='number', _min='000', _max='999',
+                                  requires=IS_MATCH('^\d{3}$',
+                                        error_message='Please enter your card\'s 3-digit security pin).'))),
+                        _class='info_div'),
                     DIV(H4('Billing Address'),
-                        DIV(LABEL('Use same as previous address?', _for='use_prev'),
-                            INPUT(_name='use_prev', _type='checkbox', _checked='true')),
                         DIV(LABEL('House Name / Number', _for='bill_house'),
-                            INPUT(_name='bill_house', requires=IS_NOT_EMPTY(),
-                                  _value=session.signup['address.house'], _readonly='true')),
+                            INPUT(_name='bill_house',
+                                  requires=IS_NOT_EMPTY(error_message='Please enter your billing house name / number.'),
+                                  _value=session.signup['address.house'])),
                         DIV(LABEL('Road name', _for='bill_street'),
-                            INPUT(_name='bill_street', requires=IS_NOT_EMPTY(),
-                                  _value=session.signup['address.street'], _readonly='true')),
+                            INPUT(_name='bill_street',
+                                  requires=IS_NOT_EMPTY(error_message='Please enter your billing address road name.'),
+                                  _value=session.signup['address.street'])),
                         DIV(LABEL('City', _for='bill_city'),
-                            INPUT(_name='bill_city', requires=IS_NOT_EMPTY(),
-                                  _value=session.signup['address.city'], _readonly='true')),
+                            INPUT(_name='bill_city',
+                                  requires=IS_NOT_EMPTY(error_message='Please enter the city of your billing address.'),
+                                  _value=session.signup['address.city'])),
                         DIV(LABEL('Country', _for='bill_country'),
-                            INPUT(_name='bill_country', requires=IS_NOT_EMPTY(),
-                                  _value=session.signup['address.country'], _readonly='true')),
+                            INPUT(_name='bill_country',
+                                  requires=IS_NOT_EMPTY(error_message='Please enter the country of your billing address.'),
+                                  _value=session.signup['address.country'])),
                         DIV(LABEL('Post Code', _for='bill_postcode'),
-                            INPUT(_name='bill_postcode', requires=IS_MATCH('^\w{4} \w{3}$'),
-                                  _value=session.signup['address.postcode'], _readonly='true'))),
-                    DIV(INPUT(_type='submit', _value='Submit')))
-        form.add_button('Prev', URL('signup', args=(session.current_signup_page - 1)))
+                            INPUT(_name='bill_postcode',
+                                  requires=IS_MATCH('^\w{4} \w{3}$',
+                                        error_message='Please enter your billing post code (e.g. YO12 5QQ).'),
+                                  _value=session.signup['address.postcode'])),
+                    _class='address_div'),
+                    DIV(_class='clear'),
+                    INPUT(_type='submit', _value='Submit', _class="button_forward"),
+                    _id='split_form')
+        form.add_button('Prev', URL('signup', args=(session.current_signup_page - 1)), _class="button_back")
 
     # If the page hasn't been accessed in this session, start afresh
     else:
         session.current_signup_page = 0
-        form = FORM(H3('Welcome to the BootUp community!'),
-                    H4('Help us get you started by choosing your community name.'),
+        form = FORM(H2('Welcome to the BootUp community!'),
+                    H3('Help us get you started by choosing your BootUp identity:'),
                     DIV(LABEL('Username', _for='username'),
-                        INPUT(_name='username', requires=IS_NOT_IN_DB(db, 'users.username'),
+                        INPUT(_name='username',
+                              requires=IS_NOT_IN_DB(db, 'users.username',
+                                    error_message='Sorry, that username\'s already taken, please choose another.'),
                               _value=session.signup['username'] if session.signup and 'username' in session.signup else '')),
                     DIV(LABEL('Password', _for='password'),
-                        INPUT(_name='password', _type='password', requires=IS_NOT_EMPTY(),
+                        INPUT(_name='password', _type='password',
+                              requires=IS_NOT_EMPTY(error_message='Please set yourself a password (cannot be blank).'),
                               _value=session.signup['password'] if session.signup and 'password' in session.signup else '')),
                     DIV(LABEL('Confirm Password', _for='confirm_pwd'),
-                        INPUT(_name='confirm_pwd', _type='password', requires=IS_EQUAL_TO(request.vars.password),
+                        INPUT(_name='confirm_pwd', _type='password',
+                              requires=IS_EQUAL_TO(request.vars.password,
+                                    error_message='Set passwords do not match.'),
                               _value=session.signup['password'] if session.signup and 'password' in session.signup else '')),
-                    DIV(INPUT(_type='submit', _value='Next')))
-        form.add_button('Cancel', URL('index'))
+                    INPUT(_type='submit', _value='Next', _class='button_forward'),
+                    _id='user_form')
+        form.add_button('Cancel', URL('index'), _class='button_back')
 
     # On receiving a valid form response
     if form.accepts(request, session):
@@ -206,6 +233,7 @@ def signup():
             response.cookies['curr_user_id']['path'] = '/'
             redirect(URL('index'))
 
+    response.files.insert(2, URL('static', 'css/signup.css'))
     return dict(form=form)
 
 
@@ -331,7 +359,7 @@ def view_profile():
                                         city=request.vars.bill_city,
                                         country=request.vars.bill_country,
                                         postcode=request.vars.bill_postcode)
-
+        session.flash = 'Details updated.'
         redirect(URL())
 
     return dict(form=form, pledges=pledges)
@@ -667,7 +695,7 @@ def view_boot():
                 pledge['value'] = db_pledge.value
                 pledge['reward'] = db_pledge.reward
                 if db_bootable.status == OPEN_FOR_PLEDGES:
-                    pledge['pledge_link'] = A('Pledge', _class='button',
+                    pledge['pledge_link'] = A('Pledge', _class='button pledge_link',
                                               callback=URL('pledge', args=db_pledge.id))
                 pledge['pledgers'] = []
 
@@ -691,6 +719,7 @@ def view_boot():
         session.flash = "Please specify the bootable you're looking for using their ID."
         redirect(URL('index'))
 
+    response.files.insert(2, URL('static', 'css/view_boot.css'))
     return bootable
 
 
@@ -760,8 +789,8 @@ def search():
     if 'query' in request.vars:
         result = []
 
-        for db_result in db((db.bootables.title.contains(request.vars.query)
-                            | db.bootables.intro.contains(request.vars.query))
+        for db_result in db((db.bootables.title.lower().contains(request.vars.query.lower())
+                            | db.bootables.intro.lower().contains(request.vars.query.lower()))
                             & (db.bootables.status != NOT_AVAILABLE))\
                 .select(limitby=(request.args[0] * 10, (request.args[0] + 1) * 10) if request.args(0) else None):
             bootable = Storage()
@@ -774,7 +803,7 @@ def search():
             bootable['total'] = db((db.pledges.boot_ref == db_result.id)
                                    & (db.pledged.pledge_ref == db.pledges.id)).select(value_sum).first()[value_sum]
 
-            if 'category' in request.vars and request.vars.category in bootable['category']:
+            if 'category' in request.vars and request.vars.category.lower() in bootable['category'].lower():
                 result.append(bootable)
             elif 'category' not in request.vars:
                 result.append(bootable)
@@ -807,13 +836,14 @@ def pledge():
         session.flash = "Please refer to the pledge you want to donate to with its ID."
         redirect(URL('index'))
 
+    m_pledge = db.pledges[pledge_id]
+    bootable = db.bootables[m_pledge.boot_ref]
+
     # Only allow signed in users to pledge
     if not 'curr_user_id' in request.cookies:
         session.flash = "Sorry, you need to be signed in to pledge!"
-        redirect(URL('index'))
+        redirect(URL('view_boot', args=bootable.id))
 
-    m_pledge = db.pledges[pledge_id]
-    bootable = db.bootables[m_pledge.boot_ref]
     if bootable.status == OPEN_FOR_PLEDGES:
         db.pledged.insert(pledge_ref=pledge_id,
                           user_ref=request.cookies['curr_user_id'].value)
@@ -821,7 +851,7 @@ def pledge():
         session.flash = "You just pledged £" + str(m_pledge.value) + " to " + bootable.title + ". Good on you. :)"
     else:
         session.flash = "Sorry, that bootable's not open for pledges right now!"
-    redirect(URL('view_boot', args=m_pledge.boot_ref))
+    redirect(URL('view_boot', args=bootable.id))
 
 
 def login():
